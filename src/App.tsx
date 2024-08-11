7c074fd7ed3c694353cdbd8a8abad87262e9004a
rifafachrir <158155145+rifafachrir@users.noreply.github.com>
2024-08-11 20:14:20 +0700

    my first Hello World on react

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Message from './Message'
+
+function App(){
+  // ada dua cara untuk memanggil Message dan semua cara itu selalu ditutup
+  // return <div><Message></Message></div> cara 1
+  return <div><Message /></div> // cara 2
+}
+
+export default App;
